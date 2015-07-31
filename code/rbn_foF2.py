4a7204e52d39c3c7d99395e9b6a75db71c81bfc0
Magda Moses <magda14@vt.edu>
2015-07-31 14:23:01 -0400

    Created main script file and library file for foF2 calculation from RBN data.

---
+++ code/rbn_foF2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#Code to calcuate and make contour plots of foF2 from RBN data over a specified region and time
+
+import sys
+import os
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+import numpy as np
+import pandas as pd
+
+from davitpy import gme
+import datetime
+
+import rbn_lib
+import handling
+
+#ePOP data input file
+inPath="data/epop"
+fname="Callsigns_7MHz.csv"
+infile = os.path.join(inPath,fname)
+print infile
+#create output directory if none exists
+output_dir='output/epop'
+#handling.prepare_output_dirs({0:output_dir},clear_output_dirs=True)
+#try: 
+#    os.makedirs(output_dir)
+#except:
+#    pass 
---
+++ code/rbn_foF2_lib.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+dxf_prop        = {'marker':'*','color':'blue'}
+dxf_leg_size    = 150
+dxf_plot_size   = 50
+
+def set_region():
+
