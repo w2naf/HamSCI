844fd4ca6933701f0ad369df87c0851162d6cf4f
Magda Moses <magda14@vt.edu>
2015-08-23 23:16:28 -0400

    Modified rbn test files to run on this machine. Created file to make pickle files of rbn data.

---
+++ code/rbn_mkPickle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#Get RBN data from a specified time interval and save data in a pickle file
+import os
+import datetime
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+import rbn_lib
+
+sTime   = datetime.datetime(2015,7,11)
+eTime   = datetime.datetime(2015,7,12,12,0)
+
+#sTime   = datetime.datetime(2015,7,11,12,0)
+#eTime   = datetime.datetime(2015,7,12,12,0)
+
+data_dir    = os.path.join('data','rbn')
+rbn_df  = rbn_lib.read_rbn(sTime,eTime,data_dir=data_dir)
+import ipdb; ipdb.set_trace()
+
+#fig     = plt.figure(figsize=(10,8))
+#ax      = fig.add_subplot(111)
+#
+#rbn_lib.rbn_map_plot(rbn_df,legend=False,ax=ax)
+#outfile = os.path.join('output','rbn','rbn_test.png')
+#fig.savefig(outfile,bbox_inches='tight')
---
+++ code/rbn_test2b.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os
+import datetime
+import pickle
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+import numpy as np
+import pandas as pd
+
+from davitpy import utils
+import rbn_lib
+
+#sTime   = datetime.datetime(2010,11,19)
+#eTime   = datetime.datetime(2010,11,19)
+#
+data_dir    = os.path.join('data','rbn')
+#rbn_df  = rbn_lib.read_rbn(sTime,eTime,data_dir=data_dir)
+
+datafile = 'rbn_201011190001-201011190002.p'
+with open(os.path.join(data_dir,datafile),'rb') as fl:
+    rbn_df = pickle.load(fl)
+
+import ipdb; ipdb.set_trace()
+rbn_df  = rbn_df.dropna(subset=['dx_lat','dx_lon','de_lat','de_lon'],how='any')
+grp     = rbn_df.groupby('callsign')
+
+
+fig     = plt.figure(figsize=(10,8))
+ax      = fig.add_subplot(111)
+
+aa  = grp.count().date
+aa  = aa.sort(inplace=False)
+
+call    = 'WZ7I'
+sub_df  = rbn_df[rbn_df.callsign == call]
+import ipdb; ipdb.set_trace()
+
+lat_lon = zip(sub_df['de_lat'].tolist(),sub_df['de_lon'].tolist(),sub_df['dx_lat'].tolist(),sub_df['dx_lon'].tolist())
+#dist    = utils.geoPack.greatCircleDist(sub_df['de_lat'],sub_df['de_lon'],sub_df['dx_lat'],sub_df['dx_lon'])
+dist    = np.array([utils.geoPack.greatCircleDist(*x) for x in lat_lon]) * utils.Re
+
+sub_df['dist'] = dist
+sub_df['mhz'] = np.floor(sub_df['freq']/1000.)
+for band,dct in rbn_lib.band_dict.iteritems():
+    tmp = sub_df[sub_df.mhz == band]
+
+    xx = np.array(tmp.date.tolist())
+    ax.plot(xx, tmp.dist, color=dct['color'],label=dct['name'])
+
+ax.legend()
+ax.set_xlim(datetime.datetime(2010,11,19,12),datetime.datetime(2010,11,19,15))
+
+outfile = os.path.join('output','rbn','rbn_test.png')
+fig.savefig(outfile,bbox_inches='tight')
+
+#fig     = plt.figure(figsize=(10,8))
+#ax      = fig.add_subplot(111)
+#
+#rbn_lib.rbn_map_plot(rbn_df,legend=False,ax=ax,plot_paths=False)
+#outfile = os.path.join('output','rbn','rbn_test.png')
+#fig.savefig(outfile,bbox_inches='tight')
