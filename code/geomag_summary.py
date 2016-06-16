c0385c4183aada88f29f256cc89bc6cb80da0b3d
Nathaniel A. Frissell <nafrissell@vt.edu>
2016-06-16 14:06:27 -0400

    start of geomagnetic summary script

---
+++ code/geomag_summary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Script to generate a summary plot of the geomagnetic environment.
+
+import os
+import datetime
+
+import numpy as np
+import scipy as sp
+
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
+
+import hamsci
+import ipdb; ipdb.set_trace()
