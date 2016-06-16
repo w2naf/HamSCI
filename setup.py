900bd58a4370e1e14fe41e165ab77f1bd7be2b0e
Nathaniel A. Frissell <nafrissell@vt.edu>
2016-06-16 14:22:46 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hamsci',
+        version='0.0.1',
+        description='Scientific Analysis Code for the Ham Radio Science Citizen Investigation',
+        author='Nathaniel A. Frissell',
+        author_email='nafrissell@vt.edu',
+        url='http://www.hamsci.org',
+        packages=['hamsci'],
+        )
