d4b10c4c9dd98a5e6a8e235e54f001b2de945bcf
Magda Moses <magda14@vt.edu>
2016-11-23 14:23:31 -0500

    Change start and end times.

--- scripts/wspr_agu.py
+++ scripts/wspr_agu.py
@@ -53,8 +53,11 @@ if __name__ == '__main__':
     sTime       = datetime.datetime(2016,11,11)
     eTime       = datetime.datetime(2016,11,18)
 
-    sTime       = datetime.datetime(2016,10,1,0)
-    eTime       = datetime.datetime(2016,10,31,23,59, 59)
+#    sTime       = datetime.datetime(2016,10,1,0)
+#    eTime       = datetime.datetime(2016,10,31,23,59, 59)
+
+    sTime       = datetime.datetime(2016,11,1,8)
+    eTime       = datetime.datetime(2016,11,2,0)
     data_dir    = 'data/wspr' 
 
     df = wspr_lib.read_wspr(sTime,eTime,data_dir)
