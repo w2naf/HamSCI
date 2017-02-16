71beac3e299ee7314388f8d2af3a573e5b1abec3
Nathaniel A. Frissell <nafrissell@vt.edu>
2017-02-16 13:49:50 -0500

    deprecating handling

--- hamsci/general_lib.py
+++ hamsci/general_lib.py
@@ -75,14 +75,49 @@ def generate_radar_dict():
 
     return radar_dict
 
-def prepare_output_dirs(output_dirs={0:'output'},clear_output_dirs=False,img_extra=''):
+class TimeCheck(object):
+    import datetime
+    #import inspect
+    #curr_file = inspect.getfile(inspect.currentframe()) # script filename (usually with path)
+    #import logging
+    #import logging.config
+    #logging.filename    = curr_file+'.log'
+    #logging.config.fileConfig("logging.conf")
+    #log = logging.getLogger("root")
+
+    def __init__(self,label=None,log=None):
+        self.label  = label
+        self.log    = log
+        self.t0     = datetime.datetime.now()
+    def check(self):
+        self.t1 = datetime.datetime.now()
+        dt      = self.t1 - self.t0
+
+        txt = '{sec}'.format(sec=str(dt))
+
+        if self.label is not None:
+            txt = ': '.join([self.label,txt])
+
+        if self.log is not None:
+            log.info(txt)
+        else:
+            print txt
+
+def prepare_output_dirs(output_dirs={0:'output'},clear_output_dirs=False,width_100=False,img_extra='',
+        php_viewers=True):
+    import os
+    import shutil
+
+    if width_100:
+        img_extra = "width='100%'"
+
     txt = []
     txt.append('<?php')
     txt.append('foreach (glob("*.png") as $filename) {')
-    txt.append('    echo "<img src=\'$filename\' width=\'100%\'> ";')
+    txt.append('    echo "<img src=\'$filename\' {img_extra}> ";'.format(img_extra=img_extra))
     txt.append('}')
     txt.append('?>')
-    show_all_txt_100 = '\n'.join(txt)
+    show_all_txt = '\n'.join(txt)
 
     txt = []
     txt.append('<?php')
@@ -90,7 +125,7 @@ def prepare_output_dirs(output_dirs={0:'output'},clear_output_dirs=False,img_ext
     txt.append('    echo "<img src=\'$filename\' {img_extra}> <br />";'.format(img_extra=img_extra))
     txt.append('}')
     txt.append('?>')
-    show_all_txt = '\n'.join(txt)
+    show_all_txt_breaks = '\n'.join(txt)
 
     for value in output_dirs.itervalues():
         if clear_output_dirs:
@@ -102,7 +137,8 @@ def prepare_output_dirs(output_dirs={0:'output'},clear_output_dirs=False,img_ext
             os.makedirs(value)
         except:
             pass
-        with open(os.path.join(value,'0000-show_all_100.php'),'w') as file_obj:
-            file_obj.write(show_all_txt_100)
-        with open(os.path.join(value,'0000-show_all.php'),'w') as file_obj:
-            file_obj.write(show_all_txt)
+        if php_viewers:
+            with open(os.path.join(value,'0000-show_all.php'),'w') as file_obj:
+                file_obj.write(show_all_txt)
+            with open(os.path.join(value,'0000-show_all_breaks.php'),'w') as file_obj:
+                file_obj.write(show_all_txt_breaks)
